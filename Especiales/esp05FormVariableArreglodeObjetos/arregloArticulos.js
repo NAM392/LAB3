11479c73d18ed1096dc497aca1519bb81b1027cf
NAM392 <status>
2020-06-05 15:27:14 -0300

    especiales

---
+++ Especiales/esp05FormVariableArreglodeObjetos/arregloArticulos.js
@@ -0,0 +1,7 @@
+var textoJSON_ART = '{"Conservas":['+
+'{"cod":"FS","descripcion":"Frutas Secas"},'+
+'{"cod":"CF","descripcion":"Conserva de Frutas"},'+
+'{"cod":"CV","descripcion":"Conserva de Verduras"},'+
+'{"cod":"CP","descripcion":"Conserva de Pescado"},'+
+'{"cod":"ES","descripcion":"Especias"}'+
+']}';
---
+++ Especiales/esp10TablaVariableArreglodeObjetos/arregloArticulos.js
@@ -0,0 +1,9 @@
+
+
+var textoJSON_ART = '{"Frutos":['+
+'{"CodArt":"nuClase1","familia":"Frutas Secas","UM":"Kg","descripcion":"Almendras non pareil peladas"},'+
+'{"CodArt":"nuClase2","familia":"Frutas Secas","UM":"Kg","descripcion":"Almendras Canarias"},'+
+'{"CodArt":"nuClase3","familia":"Frutas Secas","UM":"Kg","descripcion":"Nueces Mariposa"},'+
+'{"CodArt":"nuClase4","familia":"Frutas Secas","UM":"Kg","descripcion":"Avellana"},'+
+'{"CodArt":"nuClase5","familia":"Frutas Secas","UM":"Kg","descripcion":"Mani"}'+
+']}';
