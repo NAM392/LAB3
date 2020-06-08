7023d34575e2615728696a2a9489db3e8e9c19c3
NAM392 <status>
2020-06-08 16:07:56 -0300

    ultimos especiales

---
+++ Especiales/esp20TablaFormulario/arreglosArticulos.js
@@ -0,0 +1,12 @@
+
+
+var textoJSON_ART = '{"Frutos":['+
+'{"CodArt":"nuClase1","familia":"Frutas Secas","UM":"Kg","descripcion":"Almendras non pareil peladas","fecha":"10/10/19","saldo":"1500"},'+
+'{"CodArt":"nuClase2","familia":"Frutas Secas","UM":"Kg","descripcion":"Almendras Canarias","fecha":"12/12/19","saldo":"2800"},'+
+'{"CodArt":"nuClase3","familia":"Frutas Secas","UM":"Kg","descripcion":"Nueces Mariposa","fecha":"28/6/19","saldo":"inf"},'+
+'{"CodArt":"nuClase4","familia":"Frutas Secas","UM":"Kg","descripcion":"Avellana","fecha":"23/1/20","saldo":"1424"},'+
+'{"CodArt":"nuClase5","familia":"Frutas Secas","UM":"Kg","descripcion":"Mani","fecha":"2/2/20","saldo":"2366"}'+
+']}';
+
+
+
