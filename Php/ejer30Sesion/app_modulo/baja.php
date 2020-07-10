f44963eb66a9465939a70b79be7599bea8544d5b
NAM392 <status>
2020-07-10 19:16:43 -0300

    lala

--- Php/ejer30Sesion/app_modulo/baja.php
+++ Php/ejer30Sesion/app_modulo/baja.php
@@ -2,7 +2,7 @@
 
 session_start();
 if(!isset($_SESSION['ejercicio'])){
-	header('location:./formularioDeLogin.html');
+	header('location:../formularioDeLogin.html');
 	exit();
 }
 
--- Php/ejer30Sesion/app_modulo/modi.php
+++ Php/ejer30Sesion/app_modulo/modi.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 if(!isset($_SESSION['ejercicio'])){
-	header('location:./formularioDeLogin.html');
+	header('location:../formularioDeLogin.html');
 	exit();
 }
 
